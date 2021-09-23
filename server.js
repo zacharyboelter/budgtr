6771929ccbd592241aefc04db3c47d3f9d791446
Zachary Boelter <zboelter@gmail.com>
2021-09-23 15:13:19 -0500

    server boiler

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
-// variables
+// =======================================
+//              DEPENDENCIES
+// =======================================
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+// =======================================
+//              DATABASE
+// =======================================
 const budget = require("./models/budget");
 
 
+// =======================================
+//              MIDDLEWARE
+// =======================================
+// BODY PARSER
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 
-// ROUTES
+// Static
+app.use(express.static('public'))
+// =======================================
+//              ROUTES
+// =======================================
 // index
+app.get('/budgets', (req, res) => {
+    res.render('index.ejs', { ledger: budget });
+});
 
 // new
 
 // create
 
 // show
+app.get('/budgets/:index', (req, res) => {
+    res.render('show.ejs', {
+        ledger: budget[req.params.index]
+    });
+});
 
-
-// listen up
+// =======================================
+//              LISTENER
+// =======================================
 app.listen(PORT, () => {
     console.log(`Get that money`, PORT);
 })
