19e2450d042c91c740679e1abdbc33cd3e60d113
Zachary Boelter <zboelter@gmail.com>
2021-09-23 12:36:04 -0500

    routes

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+// variables
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const budget = require("./models/budget");
 
+
+
+// ROUTES
+// index
+
+// new
+
+// create
+
+// show
+
+
+// listen up
+app.listen(PORT, () => {
+    console.log(`Get that money`, PORT);
+})
